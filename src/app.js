f5355e03ff06f31cdaaa8f08007698c03779c28b
josemoracard <jose.cardinales@gmail.com>
2022-12-22 03:02:34 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomCard = Math.floor(Math.random() * 12);
+  const randomSuit = Math.floor(Math.random() * 4);
+
+  const card = document.querySelector(".card");
+
+  card.innerHTML = randomCard;
+
+  if (randomCard == 0) {
+    card.innerHTML = "A";
+  } else if (randomCard == 9) {
+    card.innerHTML = "J";
+  } else if (randomCard == 10) {
+    card.innerHTML = "Q";
+  } else if (randomCard == 11) {
+    card.innerHTML = "K";
+  }
+
+  if (randomSuit == 0) {
+    card.classList.add("clubs");
+  } else if (randomSuit == 1) {
+    card.classList.add("spades");
+  } else if (randomSuit == 2) {
+    card.classList.add("diamonds");
+    card.classList.add("red");
+  } else if (randomSuit == 3) {
+    card.classList.add("hearts");
+    card.classList.add("red");
+  }
 };
